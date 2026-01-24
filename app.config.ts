d9846adb134ad7f60b8de9eca647e9f2344eecf8
NinaKhai <ninaakamarulzaman@gmail.com>
2026-01-24 22:32:26 +0800

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -8,13 +8,7 @@ export default defineAppConfig({
       width: 400,
       height: 300
     },
-    header: {
-      position: 'right', // possible value are : | 'left' | 'center' | 'right'
-      logo: {
-        path: '#', // path of the logo
-        pathDark: '#', // path of the logo in dark mode, leave this empty if you want to use the same logo
-        alt: '#' // alt of the logo
-      }
+    
     },
     footer: {
       credits: {
