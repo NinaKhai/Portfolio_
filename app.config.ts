d645f3d63a45e14b5a05d6b246ae42e234875b84
NinaKhai <ninaakamarulzaman@gmail.com>
2026-01-24 22:19:55 +0800

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -26,8 +26,8 @@ export default defineAppConfig({
       message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
-      twitter: '',
-      instagram: '',
+      twitter: '#',
+      instagram: '#',
       linkedin: {
         icon: 'uil:linkedin',
         label: 'LinkedIn',
